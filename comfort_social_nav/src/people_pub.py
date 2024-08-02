adf3cdbf63f22ffe667449c4cf7aea6ebb97cac3
kriskappel <kristofer.kappel@gmail.com>
2024-08-02 01:49:38 -0300

    running simulations

--- comfort_social_nav/src/people_pub.py
+++ comfort_social_nav/src/people_pub.py
@@ -4,43 +4,64 @@ import rospy
 from gazebo_msgs.msg import ModelState, ModelStates
 from people_msgs.msg import People, Person
 from std_msgs.msg import Header
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from math import cos, sin
+
+
+# Global variable to store robot position
+robot_pos = None
+
+def amcl_pose_callback(data):
+    global robot_pos
+    robot_pos = data.pose.pose.position
 
 def model_states_callback(data):     # Callback function to handle /gazebo/modelstates messages
 
     pub_people = rospy.Publisher('/people', People, queue_size=10)
 
     people = People()
-    people.header = Header(0,rospy.Time.now(),"map")
+    people.header.frame_id = "map"
 
+    if robot_pos is None:
+        rospy.loginfo("Robot position not available")
+        return
+
+    person_near = False
 
     for i in range(len(data.name)):
         model_name = data.name[i]
         if model_name == "person1":
             model_pos = data.pose[i]
-            (roll,pitch,yaw) = euler_from_quaternion([model_pos.orientation.x, model_pos.orientation.y, model_pos.orientation.z, model_pos.orientation.w])
-            mark_x = model_pos.position.x + sin(yaw)/4;
-            mark_y = model_pos.position.y + cos(yaw)/4;
-            # model_twist = data.twist[i]
 
-            # print("Model Name: {}".format(model_name))
-            # print("Model Pose: {}".format(model_pos))
-            # print("Model Twist: {}".format(model_twist))
-            # print("\n")
+            # Check if the difference is less than 4 in x and y
+            if abs(robot_pos.x - model_pos.position.x) < 4 and abs(robot_pos.y - model_pos.position.y) < 4:
+                person_near=True
+                (roll,pitch,yaw) = euler_from_quaternion([model_pos.orientation.x, model_pos.orientation.y, model_pos.orientation.z, model_pos.orientation.w])
+                mark_x = model_pos.position.x + sin(yaw)/4;
+                mark_y = model_pos.position.y + cos(yaw)/4;
+                # mark_x = model_pos.position.x + sin(yaw)/2 ;
+                # mark_y = model_pos.position.y + cos(yaw)/2 ; 
+                # model_twist = data.twist[i]
 
-            person = Person()
-            person.name = model_name
-            person.position = model_pos.position
-            person.position.x=mark_x
-            person.position.y=mark_y
+                # print("Model Name: {}".format(model_name))
+                # print("Model Pose: {}".format(model_pos))
+                # print("Model Twist: {}".format(model_twist))
+                # print("\n")
 
-            # person.velocity.x = model_pos.orientation.x
-            # person.velocity.y = model_pos.orientation.y
+                person = Person()
+                person.name = model_name
+                person.position = model_pos.position
+                person.position.x=mark_x
+                person.position.y=mark_y
 
-            people.people.append(person)
+                # person.velocity.x = model_pos.orientation.x
+                # person.velocity.y = model_pos.orientation.y
 
-    pub_people.publish(people)
+                people.people.append(person)
+
+    if person_near:
+        pub_people.publish(people)
 
 
 
@@ -50,6 +71,8 @@ def model_states_listener():
     
     # Subscribe to /gazebo/modelstates topic
     rospy.Subscriber("/gazebo/model_states", ModelStates, model_states_callback)
+    # Subscribe to /amcl_pose topic
+    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, amcl_pose_callback)
 
     # Spin to keep the script alive
     rospy.spin()
--- comfort_social_nav/src/simulation_script.py
+++ comfort_social_nav/src/simulation_script.py
@@ -6,8 +6,10 @@ import roslaunch
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Pose, Point, Quaternion
 from actionlib_msgs.msg import GoalStatus
+from std_msgs.msg import String
 
 import os
+import time
 
 def loginfo_green(message):
     '''
@@ -45,19 +47,21 @@ def movebase_client(pose_goal):
 		return client.get_result()
 
 
-def bringup_sim(index, folder_name, sim_launch_path, nav_launch_path, pose_goal):
+def bringup_sim(index, folder_name, sim_launch_path, nav_launch_path, pose_goal, syscommand_pub):
 	logmsg = "=====RUN " + str(index) + ": starting====="
 	loginfo_green(logmsg)
+	rospy.sleep(5)
 	# rospy.loginfo(logmsg)
-
+	rospy.loginfo("GAZEBO LAUNCH STARTING")
 	gazebo_uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 	roslaunch.configure_logging(gazebo_uuid)
 	gazebo_launch = roslaunch.parent.ROSLaunchParent(gazebo_uuid, [sim_launch_path])
 	gazebo_launch.start()
 	rospy.loginfo("GAZEBO LAUNCH STARTED")
 
-	rospy.sleep(3)
+	rospy.sleep(5)
 
+	rospy.loginfo("NAV LAUNCH STARTING")
 	nav_uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 	roslaunch.configure_logging(nav_uuid)
 	nav_launch = roslaunch.scriptapi.ROSLaunch()
@@ -66,13 +70,18 @@ def bringup_sim(index, folder_name, sim_launch_path, nav_launch_path, pose_goal)
 	rospy.loginfo("NAV LAUNCH STARTED")
 
 	bags_folder = "/home/kriskappel/catkin_ws/src/wheelchair_comfortable_social_nav/comfort_social_nav/bags/" + folder_name
+	images_folder= "/home/kriskappel/catkin_ws/src/wheelchair_comfortable_social_nav/comfort_social_nav/images/" + folder_name
 
 	if not os.path.exists(bags_folder):
 		os.makedirs(bags_folder)
 		# loginfo_green(logmsg)
 		rospy.loginfo(f"Folder '{bags_folder}' created.")
+	if not os.path.exists(images_folder):
+		os.makedirs(images_folder)
+		# loginfo_green(logmsg)
+		rospy.loginfo(f"Folder '{images_folder}' created.")
 
-	bag_args = "record -O " + bags_folder + "/" + folder_name + str(index) + ".bag -a"
+	bag_args = "record -O " + bags_folder + "/" + folder_name + str(index) + ".bag /amcl_pose /cmd_vel /initialpose /move_base/cancel /move_base/current_goal /move_base/feedback /move_base/global_costmap/comfort_layer/current_value /move_base/global_costmap/social_navigation_layer/current_value /move_base/goal /move_base/local_costmap/social_navigation_layer/current_value /move_base/local_costmap/vision_layer/current_value /move_base/recovery_status /move_base/result /move_base/status /move_base_simple/goal /odom /trajectory"
 
 	rosbag_node = roslaunch.core.Node(package='rosbag', node_type='record', name='record_run', args=bag_args, output=None)
 	nav_launch.launch(rosbag_node)
@@ -85,6 +94,8 @@ def bringup_sim(index, folder_name, sim_launch_path, nav_launch_path, pose_goal)
 	loginfo_green(logmsg)
 	# rospy.loginfo(logmsg)
 	result = movebase_client(pose_goal)
+
+	syscommand_pub.publish("savegeotiff")
 	if result:
 		logmsg = "=====RUN " + str(index) + ": navigation completed====="
 		loginfo_green(logmsg)
@@ -99,7 +110,7 @@ def bringup_sim(index, folder_name, sim_launch_path, nav_launch_path, pose_goal)
 
 	try:
 		gazebo_launch.spin()
-		nav_launch.parent.spin()
+		nav_launch.spin()
 	finally:
 		# After Ctrl+C, stop all nodes from running
 		gazebo_launch.shutdown()
@@ -109,24 +120,30 @@ def bringup_sim(index, folder_name, sim_launch_path, nav_launch_path, pose_goal)
 if __name__ == '__main__':
 
 	rospy.init_node('launching_script', anonymous=True)
+	# Create a publisher for /syscommand
+	syscommand_pub = rospy.Publisher('/syscommand', String, queue_size=1)
+	rospy.sleep(1)  # Give the publisher a moment to set up
 
-	num_runs = 1
+	#---VARIABLES TO CHANGE---
 
-	bag_folder_name = "test"
+	num_runs = 50
 
-	pose_goal = Pose(Point(1.0, -8.5, 0.0), Quaternion(0.0, 0.0, 0.0, -1.0)) #Corridor3m
+	bag_folder_name = "corridor_social_nav"
+
+	pose_goal = Pose(Point(3.0, -8.5, 0.0), Quaternion(0.0, 0.0, 1.0, 0.0)) #Corridor3m
 
 	sim_launch_path = "/home/kriskappel/catkin_ws/src/wheelchair_comfortable_social_nav/nuric_wheelchair_model_02/launch/wheelchair_corridors3m_actor.launch"
 	nav_launch_path = "/home/kriskappel/catkin_ws/src/wheelchair_comfortable_social_nav/comfort_social_nav/launch/wheelchair_nav.launch"
 
+	#---------------
+
 	try:
 		for i in range(0, num_runs):
-			bringup_sim(i, bag_folder_name, sim_launch_path, nav_launch_path, pose_goal)
+			bringup_sim(i, bag_folder_name, sim_launch_path, nav_launch_path, pose_goal, syscommand_pub)
 			logmsg = "=====RUN " + str(i) + ": ended====="
 			loginfo_green(logmsg)
 			# rospy.loginfo(logmsg)
-
-
+			
 
 	except rospy.ROSInterruptException:
 		pass
